05855b1c6a22f184bc456e0c1029824fcc7529bb
tarekkhater <tarekkhater103@gmail.com>
2022-08-27 02:48:18 +0200

    update

--- pages/Req.js
+++ pages/Req.js
@@ -1,7 +1,7 @@
 import {useSession} from 'next-auth/react'
 import HomePage from '../Components/Req/HomePage'
-export default function Req() {
-    const {data: session} = useSession();
+export default function req() {
+    
   return (
     <div>
        <HomePage />
